48ea7441e4b89e54953cd2a83f4b280b415417a8
Andres Baravalle <andres@baravalle.it>
2017-06-06 11:48:49 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='alphaspider',
+      version='0.1',
+      description='Proof-of-concept spider for a well known web site.',
+      url='https://baravalle.com',
+      author='Andres Baravalle',
+      author_email='a.baravalle@uel.ac.uk',
+      license='GPLv2',
+      packages=['alphaspider'],
+      zip_safe=False)
